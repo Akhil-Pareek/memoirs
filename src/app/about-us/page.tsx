f11c22732ea106cb8102cd8b1dc48ecb5b666eb6
Akhil Pareek <akhilypareek@gmail.com>
2025-07-31 19:19:44 +0530

    final

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -3,7 +3,6 @@ import { AppAssets } from "@/constants/AppAssets";
 import Image from "next/image";
 
 export default function page() {
-  console.log("hey")
   return (
     <section>
       <Banner srcUrl={AppAssets?.aboutUsBanner} bannerLabel="About Us" />
